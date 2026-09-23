62f27501d4dcb34545c5a5640937b3cb2366a6e5
rprakash388 <prakashraju.aiengineer@gmail.com>
2026-09-23 10:46:48 +0530

    Initial check-in for Python learn

---
+++ 07-OOPs/basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(a, b):
+    return a + b
+
+def suntraction(a, b):
+    return a - b
+
+def multiply(x, y):
+    return x * y
+
+def division(x, y):
+    return x / y
+
